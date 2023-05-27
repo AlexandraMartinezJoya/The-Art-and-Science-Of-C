300723e759617ccabf89435a2fe7599e97def5e2
Alexa <za0ne78@hotmail.com>
2023-05-27 10:56:30 +0300

    Update createAHtmlFileAtAllocatedMemory.c

--- MyMemoryAllocation/createAHtmlFileAtAllocatedMemory.c
+++ MyMemoryAllocation/createAHtmlFileAtAllocatedMemory.c
@@ -1,1 +1,7 @@
-// Here we must create a file of type html. or text. We can use the built-in print_f() to print our own files
+//Here we need to declare a variable. and assign it it a binary value.
+//Next we need to print the stack trace where it saved.
+//Here we need to declare a second variable, and assign it a secondary binary value
+//Next we need to print again the stack trace of where it saves
+//Then we need to assign the second varibale the next sequence of the stack trace of the first variable.
+//To try: naming the variables directly in binary. then assign the value of hexadecimal
+//to try: naming the variables in hexadecimal and then assigning them the value in decimal
